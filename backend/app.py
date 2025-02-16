b3fad32c65fc2d46500525882bc49dd7c49703e6
Munish Lohani <munish.lohani@gmail.com>
2025-02-16 21:44:23 +0800

    first commit

---
+++ backend/app.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+db=SQLAlchemy()
+
+def create_app():
+    app=Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///./sqdb.db'
+    app.config['SECRET_KEY']="secret_key"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+    db.init_app(app=app)
+    login_manager=LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view='login'
+
+
+    
+    bcrypt=Bcrypt(app=app)
+
+
+    from models import Users
+
+    @login_manager.user_loader
+    def user_loader(id):
+       return Users.query.get(id)
+    
+
+
+
+
+    # TODO: register route
+
+
+    migrate=Migrate(app=app,db=db)
+
+
+    return app
+
+
+    
---
+++ backend/models.py
@@ -0,0 +1,30 @@
+from app import db
+from flask_login import UserMixin
+from flask_bcrypt import generate_password_hash ,check_password_hash 
+
+
+# A Users Class to store UserNames
+
+class Users(db.Model,UserMixin):
+    __tablename__="users"
+    id=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(50),nullable=False,unique=True)
+    email=db.Column(db.String(150),unique=True,nullable=False)
+    password_hash=db.Column(db.String(128),nullable=False)
+    passwords=db.relationship('Password',backref='user',lazy=True)
+
+
+    def set_password_hash(self,password):
+        self.password_hash=generate_password_hash(password)
+    
+    def check_password(self,password):
+        return check_password_hash(self.password_hash,password)
+
+
+class Password(db.Model,UserMixin):
+    id=db.Column(db.Integer,primary_key=True)
+    user_id=db.Column(db.Integer,db.ForeignKey('users.id'),nullable=False)
+    site_name=db.Column(db.String(150),nullable=False)
+    site_url=db.Column(db.String(200))
+    site_password=db.Column(db.String(150),nullable=False)
+
